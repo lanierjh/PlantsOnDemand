4b67bcceadbf16524b7d4823b7b10b02ff704ac6
Vimbisai Basvi <vibasvi@gmail.com>
2023-07-22 01:20:44 -0400

    add user info to the database

--- app.py
+++ app.py
@@ -2,8 +2,8 @@
 import os
 import pathlib
 import flask
-from flask import Flask, redirect, url_for, request, render_template, session, abort
-import secrets 
+from flask import Flask, redirect, url_for, request, render_template, session, abort, jsonify
+import secrets
 from google_auth_oauthlib.flow import Flow
 from google.oauth2.credentials import Credentials
 from google.auth.transport import requests
@@ -16,29 +16,60 @@ from cachecontrol import CacheControl
 import requests
 from pip._vendor import cachecontrol
 import google.auth.transport.requests
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from flask_sqlalchemy import SQLAlchemy
+
 
 # Creating a Flask application instance
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///PlantsOnDemand.db'
+db = SQLAlchemy(app)
+
+
+# first we wanna save the session dat
+# then we'll figure out how to retrieve the session data 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(250), unique=True, nullable=False)
+    email = db.Column(db.String(250), unique=True, nullable=False)
+    picture = db.Column(db.String(250), unique=True, nullable=False)
+    googleId = db.Column(db.String(255), unique=True, nullable=False)
+
+class users_plants(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(250), unique=True, nullable=False)
+    email = db.Column(db.String(250), unique=True, nullable=False)
+    picture = db.Column(db.String(250), unique=True, nullable=False)
+    googleId = db.Column(db.String(255), unique=True, nullable=False)
+
+
+
 
 # Generating a secret key using the secrets module (not currently used in the code)
 secret_key = secrets.token_hex(32)
 #print(secret_key)
 
+
 # Google Client ID obtained from the Google Developer Console
 GOOGLE_CLIENT_ID = "138083707001-9rt68g745qcbjuhisjumsf6hnle2111s.apps.googleusercontent.com"
+
 
 # Constructing the path for the 'client_secret.json' file using the pathlib module
 client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "client_secret.json")
 
+    
+
 # Creating a Flow instance for OAuth 2.0 authentication with Google
 flow = Flow.from_client_secrets_file(
     client_secrets_file=client_secrets_file,
-    scopes=["https://www.googleapis.com/auth/userinfo.profile", 
-            "https://www.googleapis.com/auth/userinfo.email", 
+    scopes=["https://www.googleapis.com/auth/userinfo.profile",
+            "https://www.googleapis.com/auth/userinfo.email",
             "openid"],
     redirect_uri="http://127.0.0.1:5000/callback"
 )
 # "https://www.googleapis.com/auth/user.addresses.read" maybe if we decide to add the location another useful one owould be calendar
+
 
 # Creating a decorator function that checks if the user is in session before accessing protected areas
 def login_is_required(function):
@@ -52,37 +83,43 @@ def login_is_required(function):
     return wrapper
 
 
+
+
 # Route for displaying the login page
 @app.route("/login_page")
 def login_page():
     return render_template("login.html")
 
+
 # Route for initiating the login process with Google OAuth
 @app.route("/login/google")
 def login():
+    if 'saved_plants' not in session:
+        session['saved_plants'] = []
+        print("create saved plants if user doesn't have")
     authorization_url, state = flow.authorization_url()
     session["state"] = state
     return redirect(authorization_url)
 
-@app.route('/add_plant', methods=["POST"])
-@login_is_required
-def add_plant():
-    #Get plant ID from request data
-    plant_id = request.form.get('plant_id')
-    
-    #Here, we'll try to use a session variable to simulate adding 
-    # the plant to the user's profile as a test
-    #make sure this plant id is legit
-    if plant_id:
-        if 'saved_plants' not in session:
-            session['saved_plants'] = []
-            
-        # check if if plant is already in users saved plants 
-        if plant_id not in session['saved_plants']:
-            session['saved_plants'].append(plant_id)
-            
-    return redirect(url_for('plant_search'))
 
+# Define the route to handle adding a plant
+@app.route('/add_plant', methods=['POST'])
+def add_plant():
+    if request.method == 'POST':
+        plant_id = int(request.form.get('plant_id'))
+        print(plant_id)
+        # Check if the plant ID is already in the saved_plants list
+        if plant_id in session['saved_plants']:
+            print("I added", plant_id)
+            return jsonify({'success': False, 'message': 'Plant already added.'})
+
+        print(session['saved_plants'])
+        # If the plant ID is not in the list, add it to the saved_plants list
+        session['saved_plants'].append(plant_id)
+        print(session['saved_plants'])
+        # allow session mofification to be stored
+        session.modified = True
+        return jsonify({'success': True, 'message': 'Plant added successfully.'})
 
 @app.route("/callback")
 def callback():
@@ -90,8 +127,6 @@ def callback():
     # An "endpoint" is a specific URL on a web server where data can be accessed.
     # In this case, Google's OAuth 2.0 endpoint is where the user is redirected
     # after granting permission to your application.
-
-    
 
     # Fetch the access token from Google after the user grants permission
     flow.fetch_token(authorization_response=request.url)
@@ -115,26 +150,51 @@ def callback():
         audience=GOOGLE_CLIENT_ID
     )
 
+
     # Save user information in the session for future use
     session["google_id"] = id_info.get("sub")
     session["name"] = id_info.get("name")
+    session["email"] = id_info.get("email")
+    session["picture"] = id_info.get("picture") 
     print(id_info)
     #session["email_adress"] = id_info.get("e")
-
+    user = User.query.filter_by(googleId=session["google_id"]).first()
+    # Jackson u were right it's !user cz user alone means that there is something in user, I apologize!!!
+    if not user:
+        print("we create new user") 
+        # wanna make sure we save the data into a db 
+        new_user = User(
+            googleId=session["google_id"], 
+            name=session["name"], 
+            email=session["email"],
+            picture=session["picture"]
+            )
+        db.session.add(new_user)
+        db.session.commit()
+    # if the user has an account we want to restore the previous user data 
+    if user:
+        # Get user-specific data from the database (e.g., saved plants)
+        saved_plants = saved_plants.query.filter(users_plants.id.in_(user.saved_plant_ids)).all()
+        # restore user history
+        session['saved_plants'] = saved_plants
+        return render_template("protected_area.html", user=user, saved_plants=saved_plants)
     # Redirect to the protected area after successful authentication
     return redirect("/protected_area")
 
+
 # Route to clear the local session for the user (log out)
-# just redirects to home page 
+# just redirects to home page
 @app.route("/signout")
 def logout():
     session.clear()
     return redirect("/")
 
+
 # Route for the home page
 @app.route("/")
 def home():
     return render_template('home.html')
+
 
 # Route for displaying the protected area after successful login
 @app.route("/protected_area")
@@ -142,34 +202,61 @@ def home():
 def protected_area():
     return render_template('protected_area.html')
 
+
 @app.route('/plant_search', methods=['GET', 'POST'])
 def plant_search():
     output = ''
     if request.method == 'POST':
-        
+       
         # Get the user's search query from the form data
         plant_name = request.form.get('plant_name')
         care_level = request.form.get('care_level')
-        
-        
-        # response 
-        response = requests.get('https://perenual.com/api/species-list?key=sk-nhoF64b59f3266ff91582&q='+plant_name)
+       
+       
+        # response
+        response = requests.get('https://perenual.com/api/species-list?key=sk-x2Xs64bb0058c865e1636&q='+plant_name)
+
 
         result = response.json()
         newRes = result['data']
         index = 0
-        
-        #test the output in terminal 
+       
+        #test the output in terminal
         #print("THIS IS THE RESULT: ",result)
         #print("THIS IS THE DATA SECTION WE WANT FROM RESULT", newRes)
         #print("/n"*3)
         #print(newRes)
-        
+       
         while index < len(newRes):
             id = newRes[index]['id']
-            if id <= 3000:
-                #print(id)
-                idStr = str(id)
+            idStr = str(id)
+            if care_level != 'N/A':
+                levelResponse = requests.get('https://perenual.com/api/species/details/'+idStr+'?key=sk-x2Xs64bb0058c865e1636')
+                levelResult = levelResponse.json()
+            # print(levelResult['care_level'])
+            # print(care_level, 'ojnslj')
+            # print(care_level==levelResult['care_level'])
+                if care_level == levelResult["care_level"]:
+                    print(newRes[index]['common_name'])
+                    output += idStr + '\n'
+                    #print(newRes[index]['common_name'])
+                    output += newRes[index]['common_name']
+                    output += '\n'
+                    #print(newRes[index]['scientific_name'])
+                    output += str(newRes[index]['scientific_name'])
+                    output += '\n'
+                    picture = newRes[index]['default_image']
+                    if picture == None:
+                        #print("no_url")
+                        output += 'no url' + '\n'
+                    else:
+                        #print(picture['original_url'])
+                        output += picture['original_url']
+                        output += '\n'
+                    #print()
+                    output += '\n'
+            else:
+                print(newRes[index]['common_name'])
                 output += idStr + '\n'
                 #print(newRes[index]['common_name'])
                 output += newRes[index]['common_name']
@@ -189,7 +276,7 @@ def plant_search():
                 output += '\n'
             index += 1
         #return render_template('plant_search.html', name=output)
-        
+       
         print(output)
         print("/n".split(output))
         # For demonstration purposes, let's print the search parameters.
@@ -198,17 +285,22 @@ def plant_search():
         if care_level:
             print('Search by Care Level:', care_level)
 
-    
+
+   
         # we'll put our json into a list of plants as search_results.
         search_results = [
             {plant_name: care_level}
         ]
 
+
         # Return the search_results to the template for displaying the results.
         return render_template('plant_search.html', search_results=search_results, name=output)
 
+
     # If it's a GET request, we render the plant_search.html template.
     return render_template('plant_search.html', name=output)
+
+
 
 
 @app.route('/user_profile')
@@ -216,34 +308,39 @@ def user_profile():
     # Render the user_profile.html template for the user profile page
     return render_template('user_profile.html')
 
+
 @app.route('/plant_diary')
 def plant_diary():
     # Render the plant_diary.html template for the plant diary page
     return render_template('plant_diary.html')
+
 
 @app.route('/plant_simulation')
 def plant_simulation():
     # Render the plant_simulation.html template for the plant simulation page
     return render_template('plant_simulation.html')
 
+
 @app.route('/plant_recommendations')
 def plant_recommendations():
     # Get the user's search query from the form data
         plant_name = request.form.get('plant_name')
         care_level = request.form.get('care_level')
-        
-        # response 
-        response = requests.get('https://perenual.com/api/species-list?key=sk-nhoF64b59f3266ff91582&q='+plant_name)
+       
+        # response
+        response = requests.get('https://perenual.com/api/species-list?key=sk-L9aC64b73122e37dc1596&q='+plant_name)
+
 
         result = response.json()
         newRes = result['data']
         index = 0
         output = ''
 
+
         while index < len(newRes):
             id = newRes[index]['id']
             #print(id)
-            
+           
             idStr = str(id)
             output += idStr + '\n'
             #print(newRes[index]['common_name'])
@@ -264,8 +361,10 @@ def plant_recommendations():
             output += '\n'
             index += 1
 
+
         # Return the search_results to the template for displaying the results.
         return render_template('plant_search.html', name=output)
+
 
 @app.route('/error')
 def error():
@@ -274,12 +373,24 @@ def error():
 
 
 
+with app.app_context():
+    db.create_all()
+
+
 if __name__ == "__main__":
     # Override the OAUTHLIB_INSECURE_TRANSPORT variable for local development
     # this is a quick fix to it for only local development
     os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
-    
+   
     # Setting the app's secret key for session management: might have questions about the security of this procedure?
     app.secret_key = "a16d378de074a6f025a0015ebbf8c490dbfa6e2545436920823c6248c2f53256"
     # Running the Flask application in debug mode
     app.run(debug=True)
+    
+    
+
+  
+
+
+
+  
