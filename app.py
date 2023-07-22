0e79d63d93284f758561cf5dbcf4f18255e50df2
Jackson Lanier <jackson.h.lanier@vanderbilt.edu>
2023-07-21 20:39:32 -0500

    new branch new me

--- app.py
+++ app.py
@@ -17,18 +17,23 @@ import requests
 from pip._vendor import cachecontrol
 import google.auth.transport.requests
 
+
 # Creating a Flask application instance
 app = Flask(__name__)
 
+
 # Generating a secret key using the secrets module (not currently used in the code)
 secret_key = secrets.token_hex(32)
-print(secret_key)
+#print(secret_key)
+
 
 # Google Client ID obtained from the Google Developer Console
 GOOGLE_CLIENT_ID = "138083707001-9rt68g745qcbjuhisjumsf6hnle2111s.apps.googleusercontent.com"
 
+
 # Constructing the path for the 'client_secret.json' file using the pathlib module
 client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "client_secret.json")
+
 
 # Creating a Flow instance for OAuth 2.0 authentication with Google
 flow = Flow.from_client_secrets_file(
@@ -40,19 +45,26 @@ flow = Flow.from_client_secrets_file(
 )
 # "https://www.googleapis.com/auth/user.addresses.read" maybe if we decide to add the location another useful one owould be calendar
 
+
 # Creating a decorator function that checks if the user is in session before accessing protected areas
 def login_is_required(function):
     def wrapper(*args, **kwargs):
         if "google_id" not in session:
             return abort(401)  # Authorization required
         else:
-            return function()
+            return function(*args, **kwargs)  # Pass the arguments to the decorated function
+    # Rename the endpoint to avoid conflicts unlike original
+    wrapper.__name__ = f"{function.__name__}_decorated"
     return wrapper
+
+
+
 
 # Route for displaying the login page
 @app.route("/login_page")
 def login_page():
     return render_template("login.html")
+
 
 # Route for initiating the login process with Google OAuth
 @app.route("/login/google")
@@ -61,6 +73,29 @@ def login():
     session["state"] = state
     return redirect(authorization_url)
 
+
+@app.route('/add_plant', methods=["POST"])
+@login_is_required
+def add_plant():
+    #Get plant ID from request data
+    plant_id = request.form.get('plant_id')
+   
+    #Here, we'll try to use a session variable to simulate adding
+    # the plant to the user's profile as a test
+    #make sure this plant id is legit
+    if plant_id:
+        if 'saved_plants' not in session:
+            session['saved_plants'] = []
+           
+        # check if if plant is already in users saved plants
+        if plant_id not in session['saved_plants']:
+            session['saved_plants'].append(plant_id)
+           
+    return redirect(url_for('plant_search'))
+
+
+
+
 @app.route("/callback")
 def callback():
     # This function should handle data received from Google endpoint
@@ -68,24 +103,28 @@ def callback():
     # In this case, Google's OAuth 2.0 endpoint is where the user is redirected
     # after granting permission to your application.
 
-    # Override the OAUTHLIB_INSECURE_TRANSPORT variable for local development
-    # this is a quick fix to it for only local development
-    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+
+   
+
 
     # Fetch the access token from Google after the user grants permission
     flow.fetch_token(authorization_response=request.url)
+
 
     # Checking if the state returned by Google matches the one stored in the session
     if not session["state"] == request.args["state"]:
         abort(500) # state doesn't match
 
+
     # Obtaining the credentials containing the access token and other information
     credentials = flow.credentials
+
 
     # Create a request session with caching for better performance
     request_session = requests.Session()
     cached_session = CacheControl(request_session)
     token_request = Request(session=cached_session)
+
 
     # Verify the ID token to get user information
     id_info = id_token.verify_oauth2_token(
@@ -94,14 +133,17 @@ def callback():
         audience=GOOGLE_CLIENT_ID
     )
 
+
     # Save user information in the session for future use
     session["google_id"] = id_info.get("sub")
     session["name"] = id_info.get("name")
     print(id_info)
     #session["email_adress"] = id_info.get("e")
 
+
     # Redirect to the protected area after successful authentication
     return redirect("/protected_area")
+
 
 # Route to clear the local session for the user (log out)
 # just redirects to home page
@@ -110,40 +152,182 @@ def logout():
     session.clear()
     return redirect("/")
 
+
 # Route for the home page
 @app.route("/")
 def home():
     return render_template('home.html')
 
+
 # Route for displaying the protected area after successful login
 @app.route("/protected_area")
-@login_is_required
+#@login_is_required
 def protected_area():
     return render_template('protected_area.html')
 
-@app.route('/plant_search')
+
+@app.route('/plant_search', methods=['GET', 'POST'])
 def plant_search():
-    # Render the plant_search.html template for the plant search page
-    return render_template('plant_search.html')
+    output = ''
+    if request.method == 'POST':
+       
+        # Get the user's search query from the form data
+        plant_name = request.form.get('plant_name')
+        care_level = request.form.get('care_level')
+       
+       
+        # response
+        response = requests.get('https://perenual.com/api/species-list?key=sk-x2Xs64bb0058c865e1636&q='+plant_name)
+
+
+        result = response.json()
+        newRes = result['data']
+        index = 0
+       
+        #test the output in terminal
+        #print("THIS IS THE RESULT: ",result)
+        #print("THIS IS THE DATA SECTION WE WANT FROM RESULT", newRes)
+        #print("/n"*3)
+        #print(newRes)
+       
+        while index < len(newRes):
+            id = newRes[index]['id']
+            idStr = str(id)
+            if care_level != 'N/A':
+                levelResponse = requests.get('https://perenual.com/api/species/details/'+idStr+'?key=sk-x2Xs64bb0058c865e1636')
+                levelResult = levelResponse.json()
+            # print(levelResult['care_level'])
+            # print(care_level, 'ojnslj')
+            # print(care_level==levelResult['care_level'])
+                if care_level == levelResult["care_level"]:
+                    print(newRes[index]['common_name'])
+                    output += idStr + '\n'
+                    #print(newRes[index]['common_name'])
+                    output += newRes[index]['common_name']
+                    output += '\n'
+                    #print(newRes[index]['scientific_name'])
+                    output += str(newRes[index]['scientific_name'])
+                    output += '\n'
+                    picture = newRes[index]['default_image']
+                    if picture == None:
+                        #print("no_url")
+                        output += 'no url' + '\n'
+                    else:
+                        #print(picture['original_url'])
+                        output += picture['original_url']
+                        output += '\n'
+                    #print()
+                    output += '\n'
+            else:
+                print(newRes[index]['common_name'])
+                output += idStr + '\n'
+                #print(newRes[index]['common_name'])
+                output += newRes[index]['common_name']
+                output += '\n'
+                #print(newRes[index]['scientific_name'])
+                output += str(newRes[index]['scientific_name'])
+                output += '\n'
+                picture = newRes[index]['default_image']
+                if picture == None:
+                    #print("no_url")
+                    output += 'no url' + '\n'
+                else:
+                    #print(picture['original_url'])
+                    output += picture['original_url']
+                    output += '\n'
+                #print()
+                output += '\n'
+            index += 1
+        #return render_template('plant_search.html', name=output)
+       
+        print(output)
+        print("/n".split(output))
+        # For demonstration purposes, let's print the search parameters.
+        if plant_name:
+            print('Search by Name:', plant_name)
+        if care_level:
+            print('Search by Care Level:', care_level)
+
+
+   
+        # we'll put our json into a list of plants as search_results.
+        search_results = [
+            {plant_name: care_level}
+        ]
+
+
+        # Return the search_results to the template for displaying the results.
+        return render_template('plant_search.html', search_results=search_results, name=output)
+
+
+    # If it's a GET request, we render the plant_search.html template.
+    return render_template('plant_search.html', name=output)
+
+
+
 
 @app.route('/user_profile')
 def user_profile():
     # Render the user_profile.html template for the user profile page
     return render_template('user_profile.html')
 
+
 @app.route('/plant_diary')
 def plant_diary():
     # Render the plant_diary.html template for the plant diary page
     return render_template('plant_diary.html')
+
 
 @app.route('/plant_simulation')
 def plant_simulation():
     # Render the plant_simulation.html template for the plant simulation page
     return render_template('plant_simulation.html')
 
+
 @app.route('/plant_recommendations')
 def plant_recommendations():
-    return render_template('plant_recommendations')
+    # Get the user's search query from the form data
+        plant_name = request.form.get('plant_name')
+        care_level = request.form.get('care_level')
+       
+        # response
+        response = requests.get('https://perenual.com/api/species-list?key=sk-L9aC64b73122e37dc1596&q='+plant_name)
+
+
+        result = response.json()
+        newRes = result['data']
+        index = 0
+        output = ''
+
+
+        while index < len(newRes):
+            id = newRes[index]['id']
+            #print(id)
+           
+            idStr = str(id)
+            output += idStr + '\n'
+            #print(newRes[index]['common_name'])
+            output += newRes[index]['common_name']
+            output += '\n'
+            #print(newRes[index]['scientific_name'])
+            output += str(newRes[index]['scientific_name'])
+            output += '\n'
+            picture = newRes[index]['default_image']
+            if picture == None:
+                #print("no_url")
+                output += 'no url' + '\n'
+            else:
+                #print(picture['original_url'])
+                output += picture['original_url']
+                output += '\n'
+            #print()
+            output += '\n'
+            index += 1
+
+
+        # Return the search_results to the template for displaying the results.
+        return render_template('plant_search.html', name=output)
+
 
 @app.route('/error')
 def error():
@@ -152,7 +336,14 @@ def error():
 
 
 
+
+
+
 if __name__ == "__main__":
+    # Override the OAUTHLIB_INSECURE_TRANSPORT variable for local development
+    # this is a quick fix to it for only local development
+    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+   
     # Setting the app's secret key for session management: might have questions about the security of this procedure?
     app.secret_key = "a16d378de074a6f025a0015ebbf8c490dbfa6e2545436920823c6248c2f53256"
     # Running the Flask application in debug mode
