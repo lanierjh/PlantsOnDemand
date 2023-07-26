7b5ead8cb99a6dc6f18971a34807d2016daab9a2
Jackson Lanier <jackson.h.lanier@vanderbilt.edu>
2023-07-26 15:28:34 -0500

    test files

---
+++ tests/test_basic.py
@@ -0,0 +1,25 @@
+import unittest, sys
+
+#sys.path.append('../FlaskWebProject1') # imports python file from parent directory
+from app import app #imports flask app object
+
+class BasicTests(unittest.TestCase):
+
+    # executed prior to each test
+    def setUp(self):
+        self.app = app.test_client()
+
+    ###############
+    #### tests ####
+    ###############
+
+    def test_home_page(self):
+        response = self.app.get('/home', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_register_page(self):
+        response = self.app.get('/plant_search', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+if __name__ == "__main__":
+    unittest.main()
